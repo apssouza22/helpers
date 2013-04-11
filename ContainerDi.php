55688b515969ca9898d361220df29f51876a24e4
Alexsandro Pereira <apssouza22@gmail.com>
2013-04-11 11:36:50 -0300

    Adicionado nova forma de trabalhar com DI, agora registrando tratamento de dependencias ex http://codigoaberto.info/php/34-php-uma-injecao-de-dependencia.html

--- ContainerDi.php
+++ ContainerDi.php
@@ -2,19 +2,61 @@
 
 namespace helpers;
 
-class ContainerDi {
+class ContainerDi
+{
 
-    private static function getDb() {
-        $db = new \PDO("mysql:host=localhost;dbname=phptdd", "root", "root");
-        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        return $db;
-    }
+	protected static $registry = array();
 
-    public static function getObject($name, $data = "") {
-        if ($data)
-            $objct = new $name(self::getDb(), $data);
-        else
-            $objct= new $name(self::getDb());
-        return $objct;
-    }
+	private static function getDb()
+	{
+		$db = new \PDO("mysql:host=localhost;dbname=phptdd", "root", "root");
+		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		return $db;
+	}
+
+	public static function getObject($name, $data = "")
+	{
+		if ($data)
+			$objct = new $name(self::getDb(), $data);
+		else
+			$objct = new $name(self::getDb());
+		return $objct;
+	}
+
+	/**
+	 * Adicione um novo resolvedor para a matriz registro.
+	 * @Param string $name O id
+	 * @param $resolve que cria uma instância
+	 * @Return void
+	 */
+	public static function register($name, Closure $resolve)
+	{
+		static::$registry[$name] = $resolve;
+	}
+
+	/**
+	 * Criar a instância
+	 * @ Param string $ name O id
+	 * @ Return misturado
+	 */
+	public static function resolve($name)
+	{
+		if (static::registered($name)) {
+			$name = static::$registry[$name];
+			return $name();
+		}
+
+		throw new Exception('Nothing registered with that name, fool.');
+	}
+
+	/**
+	 * Determinar se o ID é registrado
+	 * @ Param string $name O id
+	 * @ Return bool Se a ID existe ou não
+	 */
+	public static function registered($name)
+	{
+		return array_key_exists($name, static::$registry);
+	}
+
 }
